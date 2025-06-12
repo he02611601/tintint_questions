f538b6acbc305cf36074fc74d20efa0ed055af68
aki <he02611601@gmail.com>
2025-06-13 00:12:36 +0800

    初步建立專案

---
+++ example/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// 訂單相關API
+Route::resource('/orders', OrderController::class);
+// 商品相關API
+Route::resource('/products', ProductController::class);
